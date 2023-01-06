ad7786ffd0a9e6404e5f5006e7f2ce2b7f66b4f3
Hadouchi Mohamed Arbi <61359900+khringa@users.noreply.github.com>
2023-01-06 14:29:40 +0100

    Update main.c

--- dht32/main.c
+++ dht32/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "MY_DHT22.h"
+#include "DHT22.h"
 
 /* USER CODE END Includes */
 
